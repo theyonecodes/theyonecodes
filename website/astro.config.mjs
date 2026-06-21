c7b8f57447c6759964ed4492ca62895ab21eb340
theyonecodes <theyonecodes@gmail.com>
2026-06-22 01:10:30 +0530

    add GitHub Pages deploy workflow

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://theyonecodes.github.io',
   base: '/theyonecodes',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sentry({ telemetry: false }), spotlightjs(), compress(), icon()]
 });
