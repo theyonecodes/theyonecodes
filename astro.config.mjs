8e27d43570b1e5db73ad45e1864b76fb258b504c
theyonecodes <theyonecodes@gmail.com>
2026-06-22 01:14:15 +0530

    remove stale root files

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
-// https://astro.build/config
-export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
-});
